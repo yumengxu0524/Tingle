0620ef12353859a8909b04793982088a62b1cad0
Jeff Xu <66701914+yumengxu0524@users.noreply.github.com>
2024-10-15 19:47:00 -0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,12 @@
+import spacy
+
+def load_nlp_model():
+    return spacy.load('en_core_web_md')
+
+def load_diary_entries():
+    return {
+        datetime.date(2024, 6, 20): ["I hope I will be able to confide everything to you..."],
+        datetime.date(2024, 6, 21): ["Writing in a diary is a really strange experience..."],
+        # Add more entries as needed
+    }
 
